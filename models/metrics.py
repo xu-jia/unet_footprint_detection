e4e59329345bea0ef1ba6b26d3fbdf87f4029991
JiaxinXu <j.xu@upfactor.fr>
2022-03-15 14:39:20 +0100

    use dice metric

--- models/metrics.py
+++ models/metrics.py
@@ -9,12 +9,11 @@ def iou_coef(y_true,y_pred,smooth=1e-6):
     return K.mean(iou)
 
 def dice_coef(y_true, y_pred,  smooth=1e-6):
-    y_pred = tf.argmax(y_pred, axis=-1)
+    y_pred = K.cast(K.argmax(y_pred, axis=-1),tf.float32)[...,tf.newaxis]
     intersection = K.sum(y_true*y_pred,axis = [1,2,3])
-    return (2. * intersection + smooth) / (K.sum(y_true,axis = [1,2,3]) + K.sum(y_pred,axis = [1,2,3]) + smooth)
-
-def dice_coef_loss(y_true, y_pred):
-    return 1.-dice_coef(y_true, y_pred)
+    mask = K.sum(y_true,axis = [1,2,3]) + K.sum(y_pred,axis = [1,2,3])
+    dice = (2*intersection + smooth) / (mask + smooth)
+    return K.mean(dice)
 
 
 
